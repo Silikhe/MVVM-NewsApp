800afadecd7ed7403c83aeeecbb539a93b5dd25c
silikhe <silaswans@gmail.com>
2022-04-25 20:55:22 +0300

    add dependancies for mvvm

--- app/src/main/java/com/silikhe/ui/NewsActivity.kt
+++ app/src/main/java/com/silikhe/ui/NewsActivity.kt
@@ -1,11 +1,12 @@
-package com.silikhe
+package com.silikhe.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.silikhe.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
     }
 }
