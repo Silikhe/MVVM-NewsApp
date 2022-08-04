e8abac792581d384adac9083b469d122bf2806af
Silikhe <silaswans@gmail.com>
2022-08-04 10:27:37 +0300

    create database for articles

--- app/src/main/java/com/androiddevs/mvvmnewsapp/db/ArticleDatabase.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/db/ArticleDatabase.kt
@@ -1,6 +1,8 @@
 package com.androiddevs.mvvmnewsapp.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.androiddevs.mvvmnewsapp.response.Article
 import java.util.concurrent.locks.Lock
@@ -16,5 +18,16 @@ abstract  class ArticleDatabase : RoomDatabase(){
         @Volatile //other threads can immediately see when a thread changes instance
         private var instance : ArticleDatabase? = null
         private val Lock = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(Lock){
+            instance ?: createDatabase(context).also { instance = it}
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                ArticleDatabase::class.java,
+                "article_db.db"
+            ).build()
     }
 }
