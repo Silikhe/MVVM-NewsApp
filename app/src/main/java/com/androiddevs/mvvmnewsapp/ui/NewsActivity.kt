933523c191a91af76e54adcb2d58a659098328ba
Silikhe <silaswans@gmail.com>
2022-08-07 23:41:55 +0300

    add static files

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -5,14 +5,20 @@ import android.os.Bundle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.db.ArticleDatabase
+import com.androiddevs.mvvmnewsapp.repository.NewsRepository
 import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
+
+    lateinit var viewModel: NewsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
 
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
         bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModelProviderFactory.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModelProviderFactory.kt
@@ -2,7 +2,12 @@ package com.androiddevs.mvvmnewsapp.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.androiddevs.mvvmnewsapp.repository.NewsRepository
 
-class NewsViewModelProviderFactory : ViewModelProvider.Factory{
-
+class NewsViewModelProviderFactory(
+    val newsRepository: NewsRepository
+) : ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return NewsViewModel(newsRepository) as T
+    }
 }
