d15233c512007f205aa1e00694ae952b06cc3094
Silikhe <silaswans@gmail.com>
2022-08-10 23:59:36 +0300

    add repository

--- app/src/main/java/com/androiddevs/mvvmnewsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/repository/NewsRepository.kt
@@ -1,8 +1,12 @@
 package com.androiddevs.mvvmnewsapp.repository
 
+import com.androiddevs.mvvmnewsapp.api.RetrofitInstance
 import com.androiddevs.mvvmnewsapp.db.ArticleDatabase
+import java.security.CodeSigner
 
 class NewsRepository(
     val db : ArticleDatabase
 ) {
+    suspend fun getBreakingNew(countryCode: String, pageNumber: Int)=
+        RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
 }
